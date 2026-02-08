5991723da3074fa0f9329a00d3ae5863e064bbcc
Ahmad-Said-2350 <mail.saydorrahman@gmail.com>
2026-02-08 14:07:13 +0600

    5 Done

---
+++ solution_5.js
@@ -0,0 +1,33 @@
+function  analyzeText(str) {
+    if(typeof str !== "string"){
+        return "Invalid";
+    }
+
+    if(str.trim().length === 0){
+        return "Invalid";
+    }
+
+    let words = str.split(" ");
+
+
+    let longestWord = "";
+    for (let i = 0; i < words.length;i++){
+        
+        if(words[i].length > longestWord.length){
+            longestWord = words[i];
+        }
+    }
+
+    let textWithoutSpaces = str.split(" ").join("");
+    let tokenCount = textWithoutSpaces.length;
+
+    return{
+        longestWord,
+        token: tokenCount
+    };
+}
+
+let output = analyzeText(
+    "Keep coding keep shining"
+);
+console.log(output);
